eb852ab06145b4276315805fdd707c2394979c2f
kugi_masa <kugimania@gmail.com>
2026-05-26 00:09:14 +0900

    Update the externals folder name to PascalCase
    - externals → Externals

--- Settings/Add-Submodules.ps1
+++ Settings/Add-Submodules.ps1
@@ -1,10 +1,10 @@
 Write-Host "Adding external submodules ..." -ForegroundColor Cyan
 
-git submodule add https://github.com/ocornut/imgui.git externals/imgui
-git submodule add https://github.com/microsoft/DirectX-Headers.git externals/DirectX-Headers
-git submodule add https://github.com/assimp/assimp.git externals/assimp
-git submodule add https://github.com/microsoft/DirectXTex.git externals/DirectXTex
-git submodule add https://github.com/richgel999/fpng.git externals/fpng
+git submodule add https://github.com/ocornut/imgui.git Externals/imgui
+git submodule add https://github.com/microsoft/DirectX-Headers.git Externals/DirectX-Headers
+git submodule add https://github.com/assimp/assimp.git Externals/assimp
+git submodule add https://github.com/microsoft/DirectXTex.git Externals/DirectXTex
+git submodule add https://github.com/richgel999/fpng.git Externals/fpng
 
 Write-Host "Updating submodlues ..." -ForegroundColor Cyan
 git submodule update --init --recursive
